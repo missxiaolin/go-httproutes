d792c97e7da9bda36f2c06e4ac1e5e5155a33341
xiaolin <462441355@qq.com>
2019-03-16 14:26:42 +0800

    * 新增数据库连接

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,9 +1,16 @@
 package dbops
 
-import "database/sql"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func openConn() * sql.DB  {
-	
+	dbConn, err := sql.Open("mysql", "root:!@#@tcp(localhost:3306)/go_video?charset=utf8")
+	if err != nil {
+		panic(err.Error())
+	}
+	return  dbConn
 }
 
 func AddUserCredential(loginName string, pwd string) error  {
