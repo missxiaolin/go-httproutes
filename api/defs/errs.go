c45875aedcf88c13273f2beb94e8d2411dd12270
xiaolin <462441355@qq.com>
2019-03-16 11:21:32 +0800

    * 基础架构

---
+++ api/defs/errs.go
@@ -0,0 +1,17 @@
+package defs
+
+type Err struct {
+	Error string `json:"error"`
+	ErrorCode string `json:"error_code"`
+	//Message string `json:"message"`
+}
+
+type ErrorResponse struct {
+	HttpSc int
+	Error Err
+}
+
+var (
+	ErrorRequestBodyParseFailed = ErrorResponse{HttpSc: 400, Error: Err{Error: "Request body is not correct", ErrorCode: "001"}}
+	ErrorNotAuthUser = ErrorResponse{HttpSc: 401, Error: Err{Error: "User authentication failed", ErrorCode: "002"}}
+)
---
+++ api/response/response.go
@@ -0,0 +1,12 @@
+package response
+
+import (
+	"net/http"
+)
+
+func sendErrorResponse(w http.ResponseWriter) {
+}
+
+func sendNormalResponse(w http.ResponseWriter) {
+
+}
