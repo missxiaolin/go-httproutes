45fdcb6f9e14bd53ee3c923ddd82b22244fcae4d
xiaolin <462441355@qq.com>
2019-03-16 19:13:41 +0800

    * 增加video_info 数据增删改

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"time"
+	"video_server/api/defs"
+	"video_server/api/utils"
 )
 
 
@@ -49,6 +52,71 @@ func DeleteUser(loginName string, pwd string) error {
 	}
 
 	_, err = stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+
+	defer stmtDel.Close()
+	return nil
+}
+
+func AddNewVideo(aid int, name string) (*defs.VideoInfo, error) {
+	// create uuid
+	vid, err := utils.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+
+	t := time.Now()
+	ctime := t.Format("2006-01-02 15:04:05")
+	stmtIns, err := dbConn.Prepare(`INSERT INTO video_info 
+		(id, author_id, name, display_ctime) VALUES(?, ?, ?, ?)`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmtIns.Exec(vid, aid, name, ctime)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: ctime}
+
+	defer stmtIns.Close()
+	return res, nil
+}
+
+
+func GetVideoInfo(vid string) (*defs.VideoInfo, error) {
+	stmtOut, err := dbConn.Prepare("SELECT author_id, name, display_ctime FROM video_info WHERE id=?")
+
+	var aid int
+	var dct string
+	var name string
+
+	err = stmtOut.QueryRow(vid).Scan(&aid, &name, &dct)
+	if err != nil && err != sql.ErrNoRows{
+		return nil, err
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	defer stmtOut.Close()
+
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: dct}
+
+	return res, nil
+}
+
+func DeleteVideoInfo(vid string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM video_info WHERE id=?")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtDel.Exec(vid)
 	if err != nil {
 		return err
 	}
--- api/defs/apidef.go
+++ api/defs/apidef.go
@@ -4,3 +4,11 @@ type UserCredential struct {
 	Username string `json:"user_name"`
 	Pwd string `json:"pwd"`
 }
+
+// Data model
+type VideoInfo struct {
+	Id string
+	AuthorId int
+	Name string
+	DisplayCtime string
+}
