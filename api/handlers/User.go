5092c6ce533384e1cda1a124b20492d65dc92ba5
xiaolin <462441355@qq.com>
2019-03-17 18:14:03 +0800

    * 登录逻辑

--- api/handlers/User.go
+++ api/handlers/User.go
@@ -1,17 +1,42 @@
 package handlers
 
 import (
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"video_server/api/dbops"
+	"video_server/api/defs"
+	"video_server/api/response"
+	"video_server/api/session"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params)  {
-	io.WriteString(w, "Create User Handle ceshi")
+	res, _ := ioutil.ReadAll(r.Body)
+	ubody := &defs.UserCredential{}
+	if err := json.Unmarshal(res, ubody); err != nil {
+		response.SendErrorResponse(w, defs.ErrorRequestBodyParseFailed)
+		return
+	}
+
+	if err := dbops.AddUserCredential(ubody.Username, ubody.Pwd); err != nil {
+		response.SendErrorResponse(w, defs.ErrorDBError)
+		return
+	}
+
+	id := session.GenerateNewSessionId(ubody.Username)
+	su := &defs.SignedUp{Success: true, SessionId: id}
+
+	if resp, err := json.Marshal(su); err != nil {
+		response.SendErrorResponse(w, defs.ErrorInternalFaults)
+		return
+	} else {
+		response.SendNormalResponse(w, string(resp), 201)
+	}
 }
 
 func Login(w http.ResponseWriter, r *http.Request, p httprouter.Params)  {
-	usernama := p.ByName("username")
-
-	io.WriteString(w, usernama)
+	uname := p.ByName("user_name")
+	io.WriteString(w, uname)
 }
--- api/response/response.go
+++ api/response/response.go
@@ -1,12 +1,20 @@
 package response
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
+	"video_server/api/defs"
 )
 
-func sendErrorResponse(w http.ResponseWriter) {
+func SendErrorResponse(w http.ResponseWriter, errResp defs.ErrResponse) {
+	w.WriteHeader(errResp.HttpSC)
+
+	resStr, _ := json.Marshal(&errResp.Error)
+	io.WriteString(w, string(resStr))
 }
 
-func sendNormalResponse(w http.ResponseWriter) {
-
+func SendNormalResponse(w http.ResponseWriter, resp string, sc int) {
+	w.WriteHeader(sc)
+	io.WriteString(w, resp)
 }
