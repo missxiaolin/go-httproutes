9392b6d9a281bb9c68d99e3a84fa1e5780b47d28
xiaolin <462441355@qq.com>
2019-03-17 18:58:58 +0800

    * edit

--- api/dbops/internal.go
+++ api/dbops/internal.go
@@ -9,13 +9,13 @@ import (
 )
 
 func InsertSession(sid string, ttl int64, uname string) error {
-	ttlstr := strconv.FormatInt(ttl, 10)
+	ttlStr := strconv.FormatInt(ttl, 10)
 	stmtIns, err := dbConn.Prepare("INSERT INTO sessions (session_id, TTL, login_name) VALUES (?, ?, ?)")
 	if err != nil {
 		return err
 	}
 
-	_, err = stmtIns.Exec(sid, ttlstr, uname)
+	_, err = stmtIns.Exec(sid, ttlStr, uname)
 	if err != nil {
 		return err
 	}
