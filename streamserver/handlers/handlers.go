d94638a6bf9e2f4f90ce4702bbf4e0279953557a
xiaolin <462441355@qq.com>
2019-03-21 07:51:47 +0800

    * 增加上传视频逻辑

--- streamserver/handlers/handlers.go
+++ streamserver/handlers/handlers.go
@@ -2,13 +2,21 @@ package handlers
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"time"
 	"video_server/streamserver/defs"
+	"video_server/streamserver/response"
 )
 
+
+
+/**
+视频播放
+ */
 func StreamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params)  {
 	vid := p.ByName("vid-id")
 	vl := defs.VIDEO_DIR + vid
@@ -17,7 +25,7 @@ func StreamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 
 	if err != nil {
 		log.Printf("Error when try to open file: %v", err)
-		//response.SendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		response.SendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
 		return
 	}
 
@@ -26,6 +34,39 @@ func StreamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 	defer video.Close()
 }
 
+/**
+视频上传
+ */
 func UploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params)  {
-	
+	// 上传大小设置
+	r.Body = http.MaxBytesReader(w, r.Body, defs.MAX_UPLOAD_SIZE)
+	if err := r.ParseMultipartForm(defs.MAX_UPLOAD_SIZE); err != nil {
+		response.SendErrorResponse(w, http.StatusBadRequest, "File is too big")
+		return
+	}
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		log.Printf("Error when try to get file: %v", err)
+		response.SendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	// 读出file
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("Read file error: %v", err)
+		response.SendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+	}
+
+	fn := p.ByName("vid-id")
+	// 写入
+	err = ioutil.WriteFile(defs.VIDEO_DIR + fn, data, 0666)
+	if err != nil {
+		log.Printf("Write file error: %v", err)
+		response.SendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	io.WriteString(w, "Uploaded successfully")
 }
