6ef6f525f423f3b59164c406a6f1e0a90e451c97
xiaolin <462441355@qq.com>
2019-03-20 10:23:12 +0800

    * 流控 中间件

--- api/response/response.go
+++ api/response/response.go
@@ -4,10 +4,9 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"video_server/api/defs"
 )
 
-func SendErrorResponse(w http.ResponseWriter, errResp defs.ErrResponse) {
+func SendErrorResponse(w http.ResponseWriter, sc int, errMsg string) {
 	w.Header().Set("Access-Control-Allow-Origin", "*") //允许访问所有域
 	w.Header().Add("Access-Control-Allow-Headers", "*") //header的类型
 	w.Header().Set("content-type", "application/json") //返回数据格式是json
@@ -15,13 +14,4 @@ func SendErrorResponse(w http.ResponseWriter, errResp defs.ErrResponse) {
 
 	resStr, _ := json.Marshal(&errResp.Error)
 	io.WriteString(w, string(resStr))
-}
-
-func SendNormalResponse(w http.ResponseWriter, resp string, sc int) {
-	w.Header().Set("Access-Control-Allow-Origin", "*") //允许访问所有域
-	w.Header().Add("Access-Control-Allow-Headers", "*") //header的类型
-	w.Header().Set("content-type", "application/json") //返回数据格式是json
-
-	w.WriteHeader(sc)
-	io.WriteString(w, resp)
 }
--- streamserver/main.go
+++ streamserver/main.go
@@ -3,8 +3,21 @@ package main
 import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"video_server/api/response"
 	"video_server/streamserver/handlers"
 )
+
+type middleWareHandler struct {
+	r *httprouter.Router
+	l *ConnLimiter
+}
+
+func NewMiddleWareHandler(r *httprouter.Router, cc int) http.Handler {
+	m := middleWareHandler{}
+	m.r = r
+	m.l = NewConnLimiter(cc)
+	return m
+}
 
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
@@ -16,7 +29,18 @@ func RegisterHandlers() *httprouter.Router {
 	return router;
 }
 
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !m.l.GetConn() {
+		response.SendErrorResponse(w, http.StatusTooManyRequests, "Too many requests")
+		return
+	}
+
+	m.r.ServeHTTP(w, r)
+	defer m.l.ReleaseConn()
+}
+
 func main()  {
 	r := RegisterHandlers()
-	http.ListenAndServe(":9000", r)
+	mh := NewMiddleWareHandler(r, 2)
+	http.ListenAndServe(":9000", mh)
 }
