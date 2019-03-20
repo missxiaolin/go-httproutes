195fdad590ab7362daaf8a33a4045a7febff560e
xiaolin <462441355@qq.com>
2019-03-20 11:03:32 +0800

    * StreamHandler 编写逻辑

--- streamserver/defs/defs.go
+++ streamserver/defs/defs.go
@@ -1,1 +1,6 @@
 package defs
+
+const (
+	VIDEO_DIR = "./videos/"
+	MAX_UPLOAD_SIZE = 1024 * 1024 * 50 //50MB
+)
--- streamserver/handlers/handlers.go
+++ streamserver/handlers/handlers.go
@@ -2,11 +2,28 @@ package handlers
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"log"
 	"net/http"
+	"os"
+	"time"
+	"video_server/streamserver/defs"
 )
 
 func StreamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params)  {
-	
+	vid := p.ByName("vid-id")
+	vl := defs.VIDEO_DIR + vid
+
+	video, err := os.Open(vl)
+
+	if err != nil {
+		log.Printf("Error when try to open file: %v", err)
+		//response.SendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	w.Header().Set("Content-Type", "video/mp4")
+	http.ServeContent(w, r, "", time.Now(), video)
+	defer video.Close()
 }
 
 func UploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params)  {
