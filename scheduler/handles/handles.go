6523b23f1839d64577967445498861027cd2f357
xiaolin <462441355@qq.com>
2019-03-23 13:31:42 +0800

    * api部分

---
+++ scheduler/handles/handles.go
@@ -0,0 +1,26 @@
+package handles
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"video_server/scheduler/dbops"
+	"video_server/scheduler/util"
+)
+
+func VidDelRecHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params){
+	vid := p.ByName("vid-id")
+
+	if len(vid) == 0 {
+		util.SendResponse(w, 400, "video id should not be empty")
+		return
+	}
+
+	err := dbops.AddVideoDeletionRecord(vid)
+	if err != nil {
+		util.SendResponse(w, 500, "Internal server error")
+		return
+	}
+
+	util.SendResponse(w, 200, "")
+	return
+}
--- scheduler/main.go
+++ scheduler/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"video_server/scheduler/handles"
+	"video_server/scheduler/taskrunner"
+)
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/video-delete-record/:vid-id", handles.VidDelRecHandler)
+
+	return router
+}
 
 func main()  {
-	defer fmt.Println("Fourth")
-	fmt.Println("First")
-	fmt.Println("Third")
+	go taskrunner.Start()
+	r := RegisterHandlers()
+	http.ListenAndServe(":9001", r)
 }
