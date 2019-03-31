80e7cba825f8eccd1c24b30b378cc033f24cd638
xiaolin <462441355@qq.com>
2019-04-01 01:26:21 +0800

    * Fix 大小写

--- api/response/response.go
+++ api/response/response.go
@@ -7,14 +7,14 @@ import (
 	"video_server/api/defs"
 )
 
-func sendErrorResponse(w http.ResponseWriter, errResp defs.ErrResponse) {
+func SendErrorResponse(w http.ResponseWriter, errResp defs.ErrResponse) {
 	w.WriteHeader(errResp.HttpSC)
 
 	resStr, _ := json.Marshal(&errResp.Error)
 	io.WriteString(w, string(resStr))
 }
 
-func sendNormalResponse(w http.ResponseWriter, resp string, sc int) {
+func SendNormalResponse(w http.ResponseWriter, resp string, sc int) {
 	w.WriteHeader(sc)
 	io.WriteString(w, resp)
 }
