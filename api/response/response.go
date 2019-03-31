ced545d5f76bdbe460a46f1037ca22532bf39b64
xiaolin <462441355@qq.com>
2019-04-01 01:24:36 +0800

    * Fix

--- api/response/response.go
+++ api/response/response.go
@@ -4,14 +4,17 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"video_server/api/defs"
 )
 
-func SendErrorResponse(w http.ResponseWriter, sc int, errMsg string) {
-	w.Header().Set("Access-Control-Allow-Origin", "*") //允许访问所有域
-	w.Header().Add("Access-Control-Allow-Headers", "*") //header的类型
-	w.Header().Set("content-type", "application/json") //返回数据格式是json
+func sendErrorResponse(w http.ResponseWriter, errResp defs.ErrResponse) {
 	w.WriteHeader(errResp.HttpSC)
 
 	resStr, _ := json.Marshal(&errResp.Error)
 	io.WriteString(w, string(resStr))
+}
+
+func sendNormalResponse(w http.ResponseWriter, resp string, sc int) {
+	w.WriteHeader(sc)
+	io.WriteString(w, resp)
 }
